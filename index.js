bbe28cdc282f82f4dd6d5747ddae784de59fe0af
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-01-24 12:12:55 +0100

    Get info about repo

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 // set application port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
 // allow cors
-app.use(cors());
-
+app.use(cors())
 
 // parse request body
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// get repository info
+function getInfo(user, repo) {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://api.github.com/repos/${user}/${repo}`)
+    .then(({ data }) => {
+      resolve(data)
+    })
+    .catch(err => {
+      reject(err)
+    })
+  })
+}
 
 // server response
 app.get('/', (req, res) => {
@@ -22,5 +35,5 @@ app.get('/', (req, res) => {
 
 // run the server
 app.listen(port, () => {
-    console.log("Server listening at port: " + port);
+    console.log("Server listening at port: " + port)
 });
